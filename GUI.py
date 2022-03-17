a021edc08e377e7c7541528e832a1c602187986b
Simão Costa <simadinisvc@gmail.com>
2022-03-17 20:53:07 +0000

    Final Update of LOGIN

--- GUI.py
+++ GUI.py
@@ -1,11 +1,13 @@
 from PySimpleGUI import PySimpleGUI as sg
 sg.theme('DarkBlue')
+sg.popup_notify('BLOCK NOTES IS ON!', 'Please Login with your account', display_duration_in_ms = 3000 ,fade_in_duration = 1000, alpha = 0.9)
 def LogLayout(value):
     if value == 0:
         layout = [
             [sg.Text('Welcome to Block NOTES! Block your NOTES!!!XD')],
             [sg.Text('Please login with your account below or Create a New One!')],
-            [sg.Button('Create a New Account'), sg.Button('Login with an acount')]
+            [sg.Button('Create a New Account'), sg.Button('Login with an acount')],
+            
         ]
         window = sg.Window('Welcome!', layout, icon="icone\icone.ico").Finalize()
 
@@ -15,8 +17,8 @@ def LogLayout(value):
         [sg.Text('Password'), sg.Input(key='password', password_char='⚫')],
         [
             sg.Text('Your Username needs to be 3 or more characters.', key = 'WRONG-User', visible=False),
-            sg.Text('Your Username needs to be 3 or more characters and the Password needs to be 8 or more characters.', key = 'WRONG-UandP', visible=False),
-            sg.Text('Your Password needs to be 8 or more characters.', key = 'WRONG-Pass', visible=False),
+            sg.Text('Your Username needs to be 3 or more characters and the Password needs to be 5 or more characters.', key = 'WRONG-UandP', visible=False),
+            sg.Text('Your Password needs to be 5 or more characters.', key = 'WRONG-Pass', visible=False),
             sg.Text('Username or Password are wrong. Try again.', key = 'WRONG-UorP', visible=False)
         ],
         [sg.OK(), sg.Cancel()]
